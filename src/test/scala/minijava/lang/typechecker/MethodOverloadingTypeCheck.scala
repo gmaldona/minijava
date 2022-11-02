12722695680310f075995eeb0a134f172bab20ae
Gregory Maldonado <gmaldona@oswego.edu>
2022-11-02 12:16:40 -0400

    Method overloading for inherited methods unit test passing

--- src/test/scala/minijava/lang/typechecker/MethodOverloadingTypeCheck.scala
+++ src/test/scala/minijava/lang/typechecker/MethodOverloadingTypeCheck.scala
@@ -2,15 +2,16 @@ package minijava.lang.typechecker
 
 import minijava.lang.parser.ParserTest
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.must.Matchers.{a, be}
 
 class MethodOverloadingTypeCheck extends AnyFlatSpec {
 
-    "The Type Checker for method overloading" should "pass" in {
+    "The Type Checker for method overloading" should "fail" in {
         val testProgram =
             """
            class Main {
             public static void main(String[] args) {
-                System.out.println(new Main2().test());
+                System.out.println(new Main2().method(2));
             }
            }
            class Main2 {
@@ -19,18 +20,45 @@ class MethodOverloadingTypeCheck extends AnyFlatSpec {
                 a = x;
                 return a;
             }
-            public int method() {
+            public boolean method(int p) {
                 int a;
                 a = 1;
-                return a;
-            }
-            public int test() {
-              return this.method(4);
+                return false;
             }
            }
           """.stripMargin
 
+        a [Throwable] should be thrownBy {
+            ParserTest.run(testProgram)
+        }
+    }
 
-        ParserTest.run(testProgram)
+    "The Type Checker for method overloading for inherited methods" should "fail" in {
+        val testProgram =
+            """
+           class Main {
+            public static void main(String[] args) {
+                System.out.println(new Main2().method(2));
+            }
+           }
+           class Main4 {
+                public boolean method(int x) {
+                  return true;
+            }
+           }
+           class Main3 extends Main4{
+           }
+           class Main2 extends Main3 {
+            public int method(int x) {
+                int a;
+                a = x;
+                return a;
+            }
+           }
+          """.stripMargin
+
+        a [Throwable] should be thrownBy {
+            ParserTest.run(testProgram)
+        }
     }
 }

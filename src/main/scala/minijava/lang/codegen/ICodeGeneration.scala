89764c6d5e95866e912b16146f8247170ea333a6
Gregory Maldonado <gmaldona@oswego.edu>
2022-11-16 01:00:05 -0500

    Interface for code gen (wanted to list out the methods I was going to use)

--- src/main/scala/minijava/lang/codegen/ICodeGeneration.scala
+++ src/main/scala/minijava/lang/codegen/ICodeGeneration.scala
@@ -10,19 +10,22 @@
 package minijava.lang.codegen
 
 import minijava.lang.ast._
+import minijava.lang.codegen.CodeGenerationImpl.ClassWriterT
+import org.objectweb.asm.{ClassWriter, MethodVisitor}
 
 trait ICodeGeneration {
 
-    def visitProgram(program: Program): Unit
-    def visitMainClass(mainClass: MainClass): Unit
-    def visitClassDecl(classDecl: ClassDecl): Unit
+    def visitProgram(program: Program): List[ClassWriterT]
+    def visitMainClass(mainClass: MainClass): ClassWriter
+    def visitMainMethodDecl(statement: Statement, cw: ClassWriter): MethodVisitor
+    def visitMethodDecl(methodDecl: MethodDecl, cw: ClassWriter): MethodVisitor
+    def visitClassDecl(classDecl: ClassDecl): ClassWriter
     def visitVarDecl(varDecl: VarDecl): Unit
-    def visitMethodDecl(methodDecl: MethodDecl): Unit
     def visitStatementBlock(statementBlock: StatementBlock): Unit
     def visitIfStatement(ifStatement: IfStatement): Unit
     def visitWhileLoop(whileLoop: WhileLoop): Unit
     def visitForLoop(forLoop: ForLoop): Unit
-    def visitPrintStatement(printStatement: PrintStatement): Unit
+    def visitPrintStatement(printStatement: PrintStatement, methodVisitor: MethodVisitor): Unit
     def visitAssignStatement(assignStatement: AssignStatement): Unit
     def visitArrayAssignStatement(arrayAssignStatement: ArrayAssignStatement): Unit
 
